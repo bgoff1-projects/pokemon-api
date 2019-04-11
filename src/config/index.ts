585003b38fe1df8585d9a738e12052b8cf367d23
Judah Goff <bgoff1@harding.edu>
2019-04-11 02:09:55 -0500

    Updated cors to allow localhost always
    Signed-off-by: Judah Goff <bgoff1@harding.edu>

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ export const serverPort = process.env.PORT || 8000;
 
 export const logFormat = 'dev';
 
-export const reactURL = process.env.reactURL ||  'https://bgoff1-projects.github.io';
+export const reactURL = process.env.reactURL || 'https://bgoff1-projects.github.io';
 export const devURL = 'http://localhost:3000';
 export const CORSMethods = 'GET, POST, OPTIONS, PUT, PATCH, DELETE';
 export const CORSHeaders = 'X-Requested-With,Content-Type';
@@ -10,7 +10,11 @@ export const CORSHeaders = 'X-Requested-With,Content-Type';
 import { Response, Request, NextFunction } from 'express';
 
 export function allowCORS(req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Access-Control-Allow-Origin', reactURL);
+    if (req.get('host') === 'localhost:8000') {
+        res.setHeader('Access-Control-Allow-Origin', devURL);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', reactURL);
+    }
     res.setHeader('Access-Control-Allow-Methods', CORSMethods);
     res.setHeader('Access-Control-Allow-Headers', CORSHeaders);
     next();
